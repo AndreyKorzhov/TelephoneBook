dd659d2c7a776c9444d73b5001813218d83ac381
AndreyKorzhov <75386768+AndreyKorzhov@users.noreply.github.com>
2021-01-28 22:53:11 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/PhoneBook.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhoneBook.dir/main.cpp.obj"
+  "PhoneBook.exe"
+  "PhoneBook.exe.manifest"
+  "PhoneBook.lib"
+  "PhoneBook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhoneBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
